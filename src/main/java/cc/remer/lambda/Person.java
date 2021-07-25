72b1a198cdee6519d0a589248c1fe972d78debba
quarkusio <no-reply@quarkus.io>
2021-07-25 06:58:35 +0000

    Initial commit

---
+++ src/main/java/cc/remer/lambda/Person.java
@@ -0,0 +1,15 @@
+package cc.remer.lambda;
+
+public class Person {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Person setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
---
+++ src/test/java/cc/remer/lambda/LambdaHandlerTestIT.java
@@ -0,0 +1,9 @@
+package cc.remer.lambda;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class LambdaHandlerTestIT extends LambdaHandlerTest {
+
+    // Execute the same tests but in native mode.
+}
